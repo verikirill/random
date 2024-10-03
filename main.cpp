c20c8e1853d9cce7cbdda216b73f85102400a1df
Kirill <verikirill@mail.ru>
2024-10-03 15:58:18 +0300

    modified:   .gitignore modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    std::vector<int> chipinkos;
+    int n, a, k;
+    std::cin>>n;
+    for (int i = 0; i < n; i++){
+        std::cin>>a;
+        chipinkos.push_back(a);
+    }
+    std::cin>>k;
+    for (int i = 0; i < k; i++){
+        chipinkos.push_back(chipinkos[i]);
+    }
+    chipinkos.erase(chipinkos.begin(), chipinkos.begin()+ k);
+
+    std::reverse(chipinkos.begin(), chipinkos.end());
+    for (auto el : chipinkos){
+        std::cout<<el<<" ";
+    }
+    std::cout<<'\n'<<"std::reverse";
 }
